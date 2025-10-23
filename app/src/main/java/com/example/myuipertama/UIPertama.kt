a915185de24d227fe25b6e553d73bacae97e4332
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-10-23 11:27:13 +0700

    mengubah Column padding 100 dp menjadi 110 dp pada AktivitasPertama

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -4,11 +4,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    Column (modifier = Modifier.padding(top=100.dp)
-        .fillMaxSize())
+    Column (modifier = Modifier.padding(top=110.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+    }
 }
