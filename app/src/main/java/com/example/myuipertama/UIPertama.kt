412ae2371bd87d986583e6262945fcfea5592cef
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-10-23 11:53:59 +0700

    menambahkan Color pada CardDefaults pada column ACtivitasPertama

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -33,8 +34,10 @@ fun ActivitasPertama(modifier: Modifier){
             .fillMaxSize(1f)
             .padding(12.dp),
             colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )) {
 
-            )
-            )
+        }
+
     }
 }
