b50301abc8bc1797dfd9d6ccf1c5d3a31dda0db7
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-10-23 12:26:58 +0700

    menambahkan Text stringResource copy pada BottomCenter

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -67,7 +67,12 @@ fun ActivitasPertama(modifier: Modifier){
                 modifier = Modifier
                     .fillMaxSize()
             ){
-
+                Text(
+                    stringResource(R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+                )
             }
         }
     }
