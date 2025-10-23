be0f29079406e81882a857797c86649dc0462870
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-10-23 11:17:53 +0700

    membuat activitasPertama

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -1,2 +1,14 @@
 package com.example.myuipertama
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column (modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize())
+}
